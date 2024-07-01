f15546ee509b5d8822d2242f2ac60f5eb3aa0637
ISTAKHAR786 <97974124+ISTAKHAR786@users.noreply.github.com>
2024-07-01 13:05:35 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+
+let Name = "samim"
+let age = 20
+let isLoggeIn = false
+let state;
+
+// number ==> 2 to power 53 
+// bigint
+// string ==> "
+//boolean ==> "true/false"
+//null ==> standalone value
+//undifined ==>
+//symbol ==> unique
+//object
+
+console.log(typeof undefined); // undefined 
+console.log( typeof null); // object
